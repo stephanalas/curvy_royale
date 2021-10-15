5d7d3d311f368351e1843acad801274d57f71a9f
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-10-15 13:10:16 -0400

    config files created

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const babelOptions = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: 'defaults',
+        modules: 'auto',
+      },
+    ],
+  ],
+};
+
+const config = {
+  mode: isProduction ? 'production' : 'development',
+  context: path.resolve(__dirname, './src'),
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions,
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true,
+      title: 'Curvy Royale',
+      appMountId: 'app',
+      filename: 'index.html',
+      inlineSource: '.(js|css)$',
+      minify: false,
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 3000,
+    hot: true,
+  },
+};
+
+module.exports = config;
